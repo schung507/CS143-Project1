b5c1802e4178abe51ba699febb5f258905c3ba03
schung507 <schung507@gmail.com>
2016-04-17 09:58:29 -0700

    fixed query syntax

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ SELECT concat(first, " ", last)
 FROM Actor, Movie, MovieActor
 WHERE MovieActor.mid = Movie.id
 		AND Actor.id = MovieActor.aid
-		AND Movie.title = "Die Another Day"
+		AND Movie.title = "Die Another Day";
 
 -- Count of actors who are in multiple movies
 SELECT count(*)
@@ -14,13 +14,13 @@ from (
 		FROM MovieActor
 		GROUP BY aid
 		HAVING count(*) > 1
-		) T
+		) T;
 
 -- Title of movies that sells more than 1,000,000 tickets
 SELECT title
 FROM Movie, Sales
 WHERE Movie.id = Sales.mid
-		AND ticketsSold > 1000000
+		AND ticketsSold > 1000000;
 
 -- Find the highest grossing film
 SELECT title
@@ -28,7 +28,7 @@ FROM Movie, Sales
 WHERE Movie.id = Sales.mid
 		AND totalIncome > ALL (SELECT totalIncome
 								FROM Sales s
-								WHERE Sales.mid <> s.mid) -- make sure not to compare movie with itself
+								WHERE Sales.mid <> s.mid); -- make sure not to compare movie with itself
 
 -- Count of number of female and male actors who directed and were in their own movies
 SELECT Actor.sex, count(*)
@@ -36,4 +36,4 @@ FROM Actor, MovieDirector, MovieActor
 WHERE MovieDirector.did = Actor.id
 		AND MovieActor.aid = Actor.id
 		AND MovieDirector.mid = MovieActor.mid
-GROUP BY Actor.sex
+GROUP BY Actor.sex;
